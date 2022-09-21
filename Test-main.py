e9f9cb2f6f9f85aba4879432b77df170c50f85a5
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2022-09-21 23:33:34 +0200

    Demo Test-main.py

---
+++ Test-main.py
@@ -0,0 +1,16 @@
+#DEMO VERSION OF THE MAIN.py FILE TO UNDERSTAND THE FUNCTIONALITY OF THE FLASK FRAMEWORK...
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    utc_time = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+    return render_template("index.html", utc_time=utc_time)
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+    
+#THE METHOD HELLO IS CALLING THE HTML CODE FILE THAT IS IN OUR STRUCTURE OF THE APPLICATION FILE.    
